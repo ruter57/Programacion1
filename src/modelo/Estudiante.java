bef23357281335be52bc4d499c7e5f22c129cd27
Eric <Eric@DESKTOP-1PIPS59>
2016-10-17 09:21:40 +0100

    Se añade la variable fechaIngreso

--- src/modelo/Estudiante.java
+++ src/modelo/Estudiante.java
@@ -4,10 +4,16 @@ import java.util.Date;
 
 public class Estudiante extends Persona {
 	private int grupo;
+	String fechaIngreso;
 
-	public Estudiante(String nif, String nombre, char sexo, Date fecha, int grupo) {
+	public Estudiante(String nif, String nombre, char sexo, Date fecha, int grupo, String fechaIngreso) {
 		super(nif, nombre, sexo, fecha);
 		this.grupo = grupo;
+		this.fechaIngreso = fechaIngreso;
+	}
+
+	public String getFechaIngreso() {
+		return fechaIngreso;
 	}
 
 	public int getGrupo() {
